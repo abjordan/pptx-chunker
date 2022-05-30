2346204d3255f468a03af454c362344a81c59530
Alex Jordan <abjordan@gmail.com>
2022-05-30 09:55:57 -0400

    Auto-rebuild COM interface to avoid stale libraries

--- presentation_chunker.py
+++ presentation_chunker.py
@@ -10,10 +10,32 @@ import sys
 import win32com.client as win32
 from win32com.client import constants
 
+# Per https://stackoverflow.com/a/69288053
+# Per https://gist.github.com/rdapaz/63590adb94a46039ca4a10994dff9dbe
+def dispatch(app_name:str):
+    try:
+        from win32com import client
+        app = client.gencache.EnsureDispatch(app_name)
+    except AttributeError:
+        # Corner case dependencies.
+        import os
+        import re
+        import sys
+        import shutil
+        # Remove cache and try again.
+        MODULE_LIST = [m.__name__ for m in sys.modules.values()]
+        for module in MODULE_LIST:
+            if re.match(r'win32com\.gen_py\..+', module):
+                del sys.modules[module]
+        shutil.rmtree(os.path.join(os.environ.get('LOCALAPPDATA'), 'Temp', 'gen_py'))
+        from win32com import client
+        app = client.gencache.EnsureDispatch(app_name)
+    return app
+
 # Calling gencache.EnsureDispatch makes sure that pywin32 regenerates
 # the Python wrappers for the COM objects we're going to use. We need
 # this to access the constants for SaveAsPNG (for example)
-app = win32.gencache.EnsureDispatch("PowerPoint.Application")
+app = dispatch("PowerPoint.Application")
 
 filepath = Path(sys.argv[1]).resolve()
 
